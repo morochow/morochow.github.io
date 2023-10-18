09d81acdea4d10fdf2d9b25d7dc6c3763c7d094b
morochow <morochow@gmail.com>
2023-10-18 14:31:31 +0300

    life

--- loader.js
+++ loader.js
@@ -4,64 +4,12 @@ $(".js-trigger-transition").on("click", function (e) {
 });
 
 function transition() {
-  var tl = new TimelineMax();
-
-  tl.to(
-    CSSRulePlugin.getRule("body:before"),
-    0.2,
-    { cssRule: { top: "50%" }, ease: Power2.easeOut },
-    "close"
-  )
-    .to(
-      CSSRulePlugin.getRule("body:after"),
-      0.2,
-      { cssRule: { bottom: "50%" }, ease: Power2.easeOut },
-      "close"
-    )
-    .to($(".loader"), 0.2, { opacity: 1 })
-    .to(
-      CSSRulePlugin.getRule("body:before"),
-      0.2,
-      { cssRule: { top: "0%" }, ease: Power2.easeOut },
-      "+=1.5",
-      "open"
-    )
-    .to(
-      CSSRulePlugin.getRule("body:after"),
-      0.2,
-      { cssRule: { bottom: "0%" }, ease: Power2.easeOut },
-      "-=0.2",
-      "open"
-    )
-    .to($(".loader"), 0.2, { opacity: 0 }, "-=0.2");
-}
-
-var myVar;
-
-function myFunction() {
-  myVar = setTimeout(showPage, 1000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "flex";
-}
-
-$("#button").click(() => {
-  $("#loader").hide();
-});
-
-$("#button").on("click", function () {
-  window.location.href = "../dist/index.html";
-});
-
-$(".js-trigger-transition").on("click", function (e) {
-  e.preventDefault();
-  transition();
-});
-
-function transition() {
-  var tl = new TimelineMax();
+  var tl = new TimelineMax({
+    onComplete: function () {
+      // Redirect to index.html after animation completes
+      window.location.href = "../index.html";
+    },
+  });
 
   tl.to(
     CSSRulePlugin.getRule("body:before"),
@@ -102,8 +50,6 @@ function showPage() {
 // Assuming you have a button with ID "button" for navigation
 $("#button").on("click", function () {
   showPage();
-  // You may want to add more logic here if needed
-  window.location.href = "./index.html";
+  // Redirect to index.html after the animation completes
+  window.location.href = "https://morochow.github.io/index.html";
 });
-
-// LIFE
